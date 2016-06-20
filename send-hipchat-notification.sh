25da1b203ef01407a0ac0eedc77452ef592e0b62
Dmitrii Balakhonskii <dmitrii.balakhonskii@zalando.de>
2016-06-21 00:41:53 +0200

    Add notifying when color is red

--- send-hipchat-notification.sh
+++ send-hipchat-notification.sh
@@ -6,5 +6,8 @@ MESSAGE="$1"
 TITLE="${2:-}"
 COLOR="${3:-purple}"
 
+NOTIFY=false
+if [ $COLOR = red ]; then NOTIFY=true; fi
+
 curl -f -s -XPOST $RAT_HIPCHAT_URL/v2/room/$RAT_HIPCHAT_ROOM/notification?auth_token=$RAT_HIPCHAT_TOKEN \
-    -d "message=$MESSAGE&from=$TITLE&color=$COLOR"
+    -d "message=$MESSAGE&from=$TITLE&color=$COLOR&notify=$NOTIFY"
